ddfc621ea777cdb5199736dd25aa9ee5bca890c6
Shubham Raj Pandit <shubhamraj465@gmail.com>
2024-05-02 22:01:58 +0530

    Added user input

---
+++ ParkingLot.h
@@ -0,0 +1,9 @@
+#ifndef PARKINGLOT_H
+#define PARKINGLOT_H
+
+class ParkingLot {
+  private:
+    
+};
+
+#endif
---
+++ parking.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "ParkingLot.h"
+
+int main() {
+  std::cout << "This application is a simulation of a parking lot." << std::endl;
+  
+  // Asking input for the number of entrances.
+  int entrances;
+  do {
+    std::cout << "Enter the number of entrance terminals: ";
+    std::cin >> entrances;
+    if (entrances < 0 || entrances > 10) std::cout << "The number of entrances must be between 1 and 10." << std::endl;
+  }
+  while (entrances < 0 || entrances > 10);
+
+  // Asking input for the number of exits.
+  int exits;
+  do {
+    std::cout << "Enter the number of exit terminals: ";
+    std::cin >> exits;
+    if (exits < 0 || exits > 10) std::cout << "The number of exits must be between 1 and 10." << std::endl;
+  }
+  while (exits < 0 || exits > 10);
+
+  // Asking inpur for the number of parking spots.
+  int spots;
+  do {
+    std::cout << "Enter the number of parking spots: ";
+    std::cin >> spots;
+    if (spots < 10 || spots > 1000) std::cout << "The number of parking spots must be between 10 and 1000." << std::endl;
+  }
+  while (spots < 10 || spots > 1000);
+
+
+  return 0;
+}
